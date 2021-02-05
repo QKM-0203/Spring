3d30a64f1c2aa5bf9371d8545c0dcae88e968382
QKM-0203 <com_qkm@xiyoulinux.org>
2021-02-05 14:06:16 +0800

    update

--- src/main/java/sia/tacocloud/JMS/ReceiveListener.java
+++ src/main/java/sia/tacocloud/JMS/ReceiveListener.java
@@ -4,12 +4,13 @@ import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 /**
+ * 还没有安装消息代理器
  * 拉取模式和推送模式的区别就是，前者会一直等待客户端发送消息，不发就一直堵塞，而而后者是发送才接收
  */
 @Component
 public class ReceiveListener {
 
-    @JmsListener(destination = "First_Message")
+    //@JmsListener(destination = "First_Message")
     public  void dealWith(Message message){
         System.out.println(message.getText()+" "+message.getDate());
     }
