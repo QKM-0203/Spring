719258ed798f9c13b1f4086a3390d00aa0555e7e
QKM-0203 <com_qkm@xiyoulinux.org>
2021-02-08 21:46:30 +0800

    update

--- src/main/java/sia/tacocloud/Control/UserController.java
+++ src/main/java/sia/tacocloud/Control/UserController.java
@@ -1,12 +1,13 @@
 package sia.tacocloud.Control;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import sia.tacocloud.DAO.Tester;
 
+@Slf4j
 @Controller
 public class UserController {
     private Tester tester;
