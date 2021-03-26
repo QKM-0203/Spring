cb305980f8f90e7d18e97cf3e891362650665adb
QKM-0203 <com_qkm@xiyoulinux.org>
2021-03-26 17:42:12 +0800

    update

--- src/main/java/sia/tacocloud/Controller/WebConfig.java
+++ src/main/java/sia/tacocloud/Controller/WebConfig.java
@@ -10,9 +10,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
     @Override
     public void addViewControllers(ViewControllerRegistry registry){
-        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/login").setViewName("index");
 
     }
 }
